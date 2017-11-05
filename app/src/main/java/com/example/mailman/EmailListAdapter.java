0b5c46c1031c59da1e24d5c40bc4b8b3959a7dc2
Ismael <ismael.deka7899@gmail.com>
2017-11-05 13:37:18 -0500

    First prototype (11/5/2017)

---
+++ app/src/main/java/com/example/mailman/EmailListAdapter.java
@@ -0,0 +1,49 @@
+package com.example.mailman;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ismael on 11/5/2017.
+ */
+
+public class EmailListAdapter extends ArrayAdapter<Email> {
+
+
+
+    public EmailListAdapter(@NonNull Context context, ArrayList<Email> emails) {
+        super(context, 0,emails);
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.email_list_item, parent, false);
+        }
+
+        Email email = getItem(position);
+
+        TextView subjectView = (TextView) listItemView.findViewById(R.id.subject);
+        TextView snippetView = (TextView) listItemView.findViewById(R.id.snippet);
+        TextView dateView = (TextView) listItemView.findViewById(R.id.date);
+
+        subjectView.setText(email.getSubject());
+        snippetView.setText(email.getSnippet());
+        dateView.setText(email.getDate());
+
+        return listItemView;
+
+    }
+}
--- app/src/main/java/com/example/mailman/MainActivity.java
+++ app/src/main/java/com/example/mailman/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.mailman;
 
+import android.app.LoaderManager;
 import android.content.Intent;
+import android.content.Loader;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -15,15 +18,32 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.gmail.GmailScopes;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,LoaderManager.LoaderCallbacks<ArrayList<Email>>{
 
     private static final int RC_SIGN_IN = 9001;
+    private final String LOG_TAG = "Main Activity";
     private GoogleApiClient mGoogleApiClient;
+    private GoogleAccountCredential mCredential;
+    private ListView mList;
+    private ProgressBar mProgressBar;
+    private SignInButton mSignInButton;
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCredential = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), Arrays.asList(GmailScopes.GMAIL_READONLY))
+                .setBackOff(new ExponentialBackOff());
+
+        mList = (ListView) findViewById(R.id.email_list);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
@@ -32,15 +52,16 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
-        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
-        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        mSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        mSignInButton.setSize(SignInButton.SIZE_STANDARD);
 
-        signInButton.setOnClickListener(new View.OnClickListener() {
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 signIn();
             }
         });
+
 
     }
 
@@ -61,16 +82,40 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     }
 
     private void handleSignInResult(GoogleSignInResult result) {
-        Log.d("main_activity", "handleSignInResult:" + result.isSuccess());
+        Log.e(LOG_TAG, "handleSignInResult:" + result.isSuccess());
         if (result.isSuccess()) {
             // Signed in successfully, show authenticated UI.
             GoogleSignInAccount acct = result.getSignInAccount();
-            Toast.makeText(this, acct.getDisplayName(),Toast.LENGTH_LONG).show();
+            mCredential.setSelectedAccountName(acct.getEmail());
+            getLoaderManager().initLoader(1,null,this).forceLoad();
+
         }
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public Loader<ArrayList<Email>> onCreateLoader(int id, Bundle args) {
+        mProgressBar.setVisibility(View.VISIBLE);
+        mSignInButton.setVisibility(View.GONE);
+        return new MailLoader(this,mCredential,this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<ArrayList<Email>> loader, ArrayList<Email> data)
+    {
+        EmailListAdapter adapter = new EmailListAdapter(this,data);
+        mList.setAdapter(adapter);
+        mList.setVisibility(View.VISIBLE);
+        mProgressBar.setVisibility(View.GONE);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<ArrayList<Email>> loader) {
 
     }
 }
